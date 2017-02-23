7809fd19b7ae3f923aa69df3111c856ce2c45dcb
Sungmaster <mihaw.wolodymyr@gmail.com>
2017-02-23 21:43:42 +0200

    cache-to-end relative

--- index.js
+++ index.js
@@ -2,11 +2,15 @@
 
 const fs = require('fs');
 
-var data = fs.readFileSync('./me_at_the_zoo.in').toString().split("\n");
+var fileNames = ['./me_at_the_zoo.in', './videos_worth_spreading.in', './trending_today.in', './kittens.in'];
+
+var data = fs.readFileSync(fileNames[0]).toString().split("\n");
 for (var i = data.length - 1; i >= 0; i--) {
 	data[i] = data[i].split(' ').map(parseFloat);
 }
 
+
+//read task size
 var videoCount = data[0][0];
 var endPointCount = data[0][1];
 var requetCount = data[0][2];
@@ -14,18 +18,16 @@ var cacheCount = data[0][3];
 var cacheSize = data[0][4];
 
 
+//read video size
 var videoSizes = [];
-
 for (var i=0; i<data[1].length; i++){
 	videoSizes.push(data[1][i]);
 }
 
+
+//read endpoint caches
 var endPoints = [];
-
-
 var row = 2;
-
-
 for(var i=0; i<endPointCount; i++){
 	endPoints.push({data:0, cache:[], request:[]});
 
@@ -41,17 +43,37 @@ for(var i=0; i<endPointCount; i++){
 
 }
 
+//read request count
 var newRow;
 for (var i=0; i<requetCount; i++){
 
 	newRow = row + i;
 
+	if (videoSizes[data[newRow][0]] > cacheSize){data[newRow][2] = 0;}
+
 	endPoints[data[newRow][1]].request.push( {id:data[newRow][0], count:data[newRow][2]});
 
 }
 
+//sort request by count
 for (var i=0; i<endPointCount; i++){
 	endPoints[i].request.sort(  (a, b)=>(b.count-a.count) );
-
+	//console.log(endPoints[i].request);
 }
+
+var cacheToEndPoint = [];
+
+for (var i=0; i<cacheCount; i++){
+	cacheToEndPoint.push([]);	
+}
+
+for (var i=0; i<endPointCount; i++){
+	for (var j=endPoints[i].cache.length-1; j>=0; j--){
+			cacheToEndPoint[endPoints[i].cache[j].id].push(i);
+	}
+}
+
+//console.log(cacheToEndPoint);
+
+
 
