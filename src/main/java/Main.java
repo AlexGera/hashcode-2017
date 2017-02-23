25ec5048c1158c61c36b241f9f7002b95bee72e4
robben1234 <c.gavrilenko.96@gmail.com>
2017-02-23 20:40:59 +0200

    init():

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+/**
+ * <what class do>
+ *
+ * @author Kyrylo Havrylenko
+ * @see
+ */
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        System.out.println(a);
+    }
+
+}
